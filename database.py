9864fe07ddbc49f7e21933f3241a16fdd0cf3889
ajaylovesu <ajaykushwaha9016@gmail.com>
2026-05-29 14:18:46 +0530

    Database

--- database.py
+++ database.py
@@ -2,9 +2,11 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os 
 
-DATABASE_URL = "mysql+pymysql://root:ajaydroper@localhost/emp_db"
-
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
